a2f9b1983f4bcd03d54035d87a482b2e4b3e5ae0
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-08-25 23:14:30 +0300

    Update 1_do_one_thing_after.py

--- 1_do_one_thing_after.py
+++ 1_do_one_thing_after.py
@@ -1,1 +1,30 @@
+from dataclasses import dataclass
+from datetime import datetime
 
+@dataclass
+class Customer:
+  name: str
+  phone: str
+  cc_number: str
+  cc_exp_month: int
+  cc_exp_year: int
+  cc_valid: bool = False
+
+def validate_card(customer: Customer) -> bool:
+  def digits_of(number: str) -> list[int]:
+    retun [int(d) for d in number]
+
+  digits = digits_of(customer.cc_number)
+  odd_digits = digits[-1::-2]
+  even_digits = digits[-2::-2]
+  checksum = 0
+  checksum += sum(odd_digits)
+  for digit in even_digits:
+    checksum += sum(digits_of(str(digit * 2)))
+
+  customer.cc_valid = (
+    checksum % 10 == 0
+    and datetime(customer.cc_exp_year, customer.cc_exp_month, 1) > datetime.now()
+  )
+  return customer.cc_valid
+  
