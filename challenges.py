d982731971b791d9c57ef33a768ce4f375e82cd1
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-09-01 23:25:21 +0300

    Update challenges.py

--- challenges.py
+++ challenges.py
@@ -1,4 +1,4 @@
-# List Comprehension
+# List Comprehension 
 # Create a list of matches from the teams list, making sure there's no repetaion in the matches
 teams = ['wolves', 'foxes', 'tigers', 'lions', 'monkeys']
 matches = [(f"{team1} vs {team2}") for team1 in teams for team2 in teams[teams.index(team1) + 1:]]
